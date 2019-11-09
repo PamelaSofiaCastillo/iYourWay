386317f48a645f370eba5eabed6d9e702b009426
DashTennyson <rojas71421@gmail.com>
2019-11-09 10:47:58 -0500

    Initial Commit

---
+++ YourWay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YourWay
+//
+//  Created by Alumnos on 11/9/19.
+//  Copyright © 2019 yourway. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
